39cf9ce5a94811452bf874fcebd95389676afcab
dogiiits <lanka.likhith.15@gmail.com>
2019-12-24 10:09:35 +0530

    detailview

--- bookmark_manager/manager/views.py
+++ bookmark_manager/manager/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 # Create your views here.
 from .models import Post
 
 class ManagerListView(ListView):
     model = Post
     template_name = 'home.html'
+
+class ManagerDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
