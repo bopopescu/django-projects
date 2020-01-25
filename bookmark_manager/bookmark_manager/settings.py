78f298955ab51f39b5bebc0b3a16ce28260e5e00
likhith lanka <lanka.likhith.15@gmail.com>
2020-01-25 12:06:29 +0530

    create models

--- bookmark_manager/bookmark_manager/settings.py
+++ bookmark_manager/bookmark_manager/settings.py
@@ -14,7 +14,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'lanka.likhith.15@gmail.com'
-EMAIL_HOST_PASSWORD = 'likhithlanka123'
+EMAIL_HOST_PASSWORD = 'This is a wrong password'
 
 
 import os
